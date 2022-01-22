63f9c60d809649feefa90326edde3bf525d04412
ben-huangus <uclaidiot@gmail.com>
2022-01-22 11:06:01 -0600

    modified on 1/22/2022

--- main.py
+++ main.py
@@ -1,36 +1,30 @@
 
 import apache_beam as beam
 from apache_beam.io import WriteToText
+from apache_beam import io
+from apache_beam.dataframe import frame_base
+from apache_beam.io import fileio
+import pandas as pd
+# pytype: skip-file
 
-# class MergeCSVFn(beam.DoFn):
-#     def process(self, element):
-#         print(element)
-#         return [len(element)]
+import argparse
+import logging
+
+import apache_beam as beam
+from apache_beam.pvalue import AsSingleton
+
 
 if __name__ == '__main__':
 
-    with beam.Pipeline() as pipeline:
+    with beam.Pipeline() as p:
+        data = p | beam.io.ReadFromText(
+                  'gs://york-project-bucket/b_huang_files/2021-12-17-01-03/part-r-*')
 
-          ## C. Read from multiple files:
-        files = pipeline | beam.io.ReadFromText(
-              'gs://york-project-bucket/b_huang_files/2021-12-17-01-03/part-r-*')
-        #mergedData = files | beam.Map(print)
-          ## OR: using the Class:
-        #mergedData = files | beam.ParDo(MergeCSVFn())
+        data | 'Filter header' >> beam.Filter(lambda df: df[0] == 'author')
+        data | 'Write' >> WriteToText('./output1.txt')
 
-          ## do count:
-        count = files | 'Count all elements' >> beam.combiners.Count.Globally()
-        count | beam.Map(print)
-
-        files | beam.io.WriteToText('gs://york-project-bucket/b_huang_files/2021-12-20-16-35/textFileCombined-test1')
-
-          ## 1st task "read" file from bucket:
-          # lines | "Write" >> WriteToText('./output.txt')
-
-          ## 2nd task "write" it back to bucket:
-        #lines | beam.io.WriteToText('gs://york-project-bucket/b_huang_files/2021-12-20-16-35')
-
-        pass
+        cnt = data | 'Count all element' >> beam.combiners.Count.Globally()
+        cnt | beam.Map(print)
 
 
 
